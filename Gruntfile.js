27ceec02815d43f99472d0c162f57935f85b63a2
Wayne Keenan <wayne.keenan@gmail.com>
2016-03-04 21:30:19 +0000

    creating dist tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,33 @@
 module.exports = function(grunt) {
 
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
+        pkg: grunt.file.readJSON('package.json'),
+
+        clean: ['dist'],
+
+        copy: {
+            all: {
+                expand: true,
+                cwd: "src/",
+                src: ['*.css', 'images/**/*', 'img/**/*', '!Gruntfile.js', 'conf/**/*', 'js/**/*', '*.html'],
+                dest: 'dist/',
+            },
+
+            bower: {
+                expand: true,
+                cwd: "bower_components",
+                src: ['**/*'],
+                dest: 'dist/bower_components',
+            },
+        },
+
+        vulcanize: {
+            options: {},
+            files: {
+                'dist/build.html': 'dist/index.html'
+            },
+        },
+
         connect: {
             server: {
                 options: {
@@ -13,14 +39,30 @@ module.exports = function(grunt) {
                     //cert: grunt.file.read('server.crt').toString(),
                     //ca: grunt.file.read('ca.crt').toString(),
                     keepalive: true,
-                    base: '.'
+                    base: 'dist'
                 },
             },
         },
+
+        'gh-pages': {
+            options: {
+                base: 'dist'
+            },
+            src: ['**']
+        }
 	});
 
 
-	grunt.loadNpmTasks('grunt-contrib-connect');
-	grunt.registerTask('default', 'connect:server');
+
+    // This:
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+    // Replaces lots of these:
+    //grunt.loadNpmTasks('grunt-contrib-connect');
+    //grunt.loadNpmTasks('grunt-...
+
+
+    // Default task(s).
+    grunt.registerTask('default', ['clean',  'copy', 'vulcanize' ]); //'connect:server']);
+
 
 };
